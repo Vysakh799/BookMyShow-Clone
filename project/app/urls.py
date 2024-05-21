9b16efd5106a0403755cdeb692bb43be93059ac6
vysakh7995 <vysakh7995@gmail.com>
2024-05-21 17:54:20 +0530

    added files

--- project/app/urls.py
+++ project/app/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from .import views
 
 urlpatterns=[
-    path("",views.index)
+    path('login',views.login),
+    path('logout',views.logout1),
+    path('',views.index),
+    path("see_all",views.see_all),
+    path("movie",views.movie),
+    path('register',views.register),
 ]
--- project/app/views.py
+++ project/app/views.py
@@ -1,5 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User,auth
+from django.contrib import messages
 
-# Create your views here.
+# Login page
+def login(request):
+    if request.method=='POST':
+        username=request.POST['username']
+        password=request.POST['password']
+        user=auth.authenticate(username=username,password=password)
+        if user is not None:
+            request.session['user']=username
+            auth.login(request,user)
+            return redirect(index)
+        else :
+            return redirect(login)
+    else:
+        return render(request,'login.html')
+def logout1(request):
+    # if 'user' in request.session:
+        # del request.session['user']
+        auth.logout(request)
+        print(request.user)
+
+        return redirect(index)
+    # else:
+        return redirect(login)
+def register(request):
+    if request.method=='POST':
+        username=request.POST['username']
+        email=request.POST['email']
+        password=request.POST['password']
+        data=User.objects.create_user(username=username,email=email,password=password)
+        data.save()
+        return redirect(login)
+    else:
+        return render(request,'register.html')
+# pages
 def index(request):
     return render(request,'index.html')
+def see_all(request):
+    return render(request,'see_all.html')
+def movie(request):
+    return render(request,'movie.html')
