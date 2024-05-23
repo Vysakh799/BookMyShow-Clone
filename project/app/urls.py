581dfd94423b0a60a0883d1173bce8c95caa1726
Vysakh7995 <vysakh7995@gmail.com>
2024-05-23 17:53:32 +0530

    adding files

--- project/app/urls.py
+++ project/app/urls.py
@@ -5,6 +5,7 @@ urlpatterns=[
     path('login',views.login),
     path('logout',views.logout1),
     path('',views.index),
+    path("see_all/<lang>",views.see_all),
     path("see_all",views.see_all),
     path("movie/<name>",views.view_movie),
     path('register',views.register),
--- project/app/views.py
+++ project/app/views.py
@@ -37,13 +37,34 @@ def register(request):
         return render(request,'register.html')
 # pages
 def index(request):
-    movie1=movie.objects.all()
+    movie1=movie.objects.all()[0:8]
+
     return render(request,'index.html',{'movie':movie1})
-def see_all(request):
-    return render(request,'see_all.html')
+def see_all(request,lang=None):
+    movie1=[]
+    if lang:
+        l=language.objects.filter(language=lang)
+        for i in l:
+            movie1.append(i.movie_name) 
+    else:
+        movie1=movie.objects.all()
+    lang2=[]
+    for i in language.objects.all():
+        if i.language not in lang2:
+            lang2.append(i.language)
+    print(lang2)
+    return render(request,'see_all.html',{'movie':movie1,'lang':lang,'lang2':lang2})
 def view_movie(request,name):
     movie1=movie.objects.get(name=name)
     lang=language.objects.filter(movie_name=movie1)
-    cast=movie_members.objects.filter(movie_name=movie1)
-    print(cast)
-    return render(request,'movie.html',{'movie':movie1,'lang':lang,'cast':cast})
+    data=movie_members.objects.filter(movie_name=movie1)
+    cast=[]
+    crew=[]
+    for i in data:
+        if i.member_name.type=='cast':
+            cast.append(i)
+        else:
+            crew.append(i)
+    print(cast,crew)
+    movies=movie.objects.all()
+    return render(request,'movie.html',{'movie':movie1,'lang':lang,'cast':cast,'crew':crew,'movies':movies})
